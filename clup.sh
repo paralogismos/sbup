ed133e74f828f3b1dd8529835690ac86425cf86e
David Bowling <mailbowling@gmail.com>
2025-06-01 21:34:18 -0500

    added initial draft of script

---
+++ clup.sh
@@ -0,0 +1,43 @@
+#! /usr/bin/env sh
+# clup.sh
+# A simple script for checking, building, and installing SBCL.
+set -e
+
+clup_version=0.1.0
+
+# Get installed version number.
+sbcl_installed=$(sbcl --version)
+sbcl_installed=${sbcl_installed##*[ ]}
+
+# Get a download url.
+sbcl_download="https://sourceforge.net/projects/sbcl/files/latest/download"
+sbcl_redirect=$(curl --head --silent --write-out "%{redirect_url}" --output /dev/null $sbcl_download)
+
+# Get latest version number.
+sbcl_latest=${sbcl_redirect##*/sbcl/}
+sbcl_latest=${sbcl_latest%%/*}
+
+if [ $sbcl_latest = $sbcl_installed ]
+then
+    echo "Latest version of SBCL already installed: $sbcl_latest"
+elif [ $sbcl_latest \< $sbcl_installed ]
+then
+    echo "Newer version of SBCL already installed: $sbcl_latest < $sbcl_installed"
+else
+    echo "Downloading SBCL $sbcl_latest..."
+    curl -L $sbcl_redirect --remote-name
+    sbcl_file=${sbcl_redirect##*$sbcl_latest/}
+    sbcl_file=${sbcl_file%%\?*}
+    echo "Unpacking SBCL $sbcl_latest..."
+    tar -xvf $sbcl_file
+    sbcl_dir=sbcl-$sbcl_latest
+    echo "Building SBCL $sbcl_latest..."
+    cd $sbcl_dir && ./make.sh --fancy
+    echo "Running SBCL $sbcl_latest tests..."
+    cd tests && ./run-tests.sh
+    echo "Building SBCL $sbcl_latest documentation..."
+    cd ../doc/manual && make
+    echo "Installing SBCL $sbcl_latest..."
+    cd ../.. && sudo ./install.sh
+fi
+
